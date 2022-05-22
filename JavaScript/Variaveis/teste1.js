517915f661476073320be3cb0921f791c2a2a344
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-22 13:38:48 -0300

    Imprimindo o que cada parte do teste está fazendo.

--- JavaScript/Variaveis/teste1.js
+++ JavaScript/Variaveis/teste1.js
@@ -1,64 +1,78 @@
 // Declarando variável nome
+console.log("Declarando variável nome");
 {
-    let nome = 'Henrique';
-    console.log(nome);
+  let nome = "Henrique";
+  console.log(nome);
 }
+console.log();
 
 // Testando escopo
+console.log("Testando escopo");
 {
-    {
-        let nome = 'Henrique';
-        console.log(nome);
-    }
+  {
+    let nome = "Henrique";
     console.log(nome);
+  }
+  console.log(nome);
 }
+console.log();
 
 // Testando escopo com var
+console.log("Testando escopo com var");
 {
-    {
-        var nome = 'Henrique';
-        console.log(nome);
-    }
+  {
+    var nome = "Henrique";
     console.log(nome);
+  }
+  console.log(nome);
 }
+console.log();
 
 // Testando escopo com var e let
+console.log("Testando escopo com var e let");
 {
-    {
-        var nome = 'João';
-        console.log(nome);
-    }
-
+  {
+    var nome = "João";
     console.log(nome);
+  }
 
-    {
-        let nome = 'José';
-        console.log(nome)
-    }
+  console.log(nome);
 
+  {
+    let nome = "José";
     console.log(nome);
+  }
+
+  console.log(nome);
 }
+console.log();
 
 // Testando a declaração de uma constante
+console.log("Testando a declaração de uma constante");
 {
-    const PI = 3.14159;
-    console.log(PI);
+  const PI = 3.14159;
+  console.log(PI);
 }
+console.log();
 
 // Testando variável
+console.log("Testando variável");
 {
-    let n = 2;
-    console.log(n);
+  let n = 2;
+  console.log(n);
 
-    n = 3;
-    console.log(n);
+  n = 3;
+  console.log(n);
 }
+console.log();
 
 // Testando redefinição do valor de uma constante
+console.log("Testando redefinição do valor de uma constante");
 {
-    const PI = 3.14159;
-    console.log(PI);
+  const PI = 3.14159;
+  console.log(PI);
 
-    PI = 4;
-    console.log(PI);
+  PI = 4;
+  console.log(PI);
 }
+console.log();
