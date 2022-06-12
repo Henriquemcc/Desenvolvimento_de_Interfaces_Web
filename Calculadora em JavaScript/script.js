61ab65b8125913b84b86f8fc2313412226a7df70
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-12 14:10:23 -0300

    Aula: 'Mão na Massa - Calculadora em JavaScript': Vídeo: 28:41

--- Calculadora em JavaScript/script.js
+++ Calculadora em JavaScript/script.js
@@ -1,18 +1,38 @@
-﻿function digito (n){
-    alert(n);
+﻿// Variáveis para armazenamos o valor, o operador e o estado da calculadora
+let sValor = "0";
+let ehNovoNumero = true;
+
+/**
+ * Atualiza o display da calculadora.
+ */
+function atualizarVisor() {
+  document.getElementById("display").innerText = sValor;
+}
+
+/**
+ * Executa o tratamento, quando o um botão de um número é clicado.
+ * @param {*} n Número pressionado.
+ */
+function digito(n) {
+  if (ehNovoNumero) {
+    sValor = "" + n;
+    ehNovoNumero = false;
+  } else {
+    sValor += n;
+  }
+  atualizarVisor();
 }
 
 onload = () => {
-
-    // Adicionando ação aos botões de números
-    document.getElementById("bt-7").onclick = () => digito(7);
-    document.getElementById("bt-8").onclick = () => digito(8);
-    document.getElementById("bt-9").onclick = () => digito(9);
-    document.getElementById("bt-4").onclick = () => digito(4);
-    document.getElementById("bt-5").onclick = () => digito(5);
-    document.getElementById("bt-6").onclick = () => digito(6);
-    document.getElementById("bt-1").onclick = () => digito(1);
-    document.getElementById("bt-2").onclick = () => digito(2);
-    document.getElementById("bt-3").onclick = () => digito(3);
-    document.getElementById("bt-0").onclick = () => digito(0);
-}
+  // Adicionando ação aos botões de números
+  document.getElementById("bt-7").onclick = () => digito(7);
+  document.getElementById("bt-8").onclick = () => digito(8);
+  document.getElementById("bt-9").onclick = () => digito(9);
+  document.getElementById("bt-4").onclick = () => digito(4);
+  document.getElementById("bt-5").onclick = () => digito(5);
+  document.getElementById("bt-6").onclick = () => digito(6);
+  document.getElementById("bt-1").onclick = () => digito(1);
+  document.getElementById("bt-2").onclick = () => digito(2);
+  document.getElementById("bt-3").onclick = () => digito(3);
+  document.getElementById("bt-0").onclick = () => digito(0);
+};
