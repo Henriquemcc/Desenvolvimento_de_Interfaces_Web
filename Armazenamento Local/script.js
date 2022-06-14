101a528efcb902dd10fad98282d357fb7f1d0df8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-14 17:31:48 -0300

    Aula: 'Armazenamento Local': Vídeo: 17:48

--- Armazenamento Local/script.js
+++ Armazenamento Local/script.js
@@ -1,13 +1,42 @@
 window.onload = () => {
-  // Obtendo o valor do nome do usuário armazenado localmente
-  let nome = document.getElementById("nome");
-  nome.value = window.localStorage.getItem("username");
+  // Obtendo objeto user
+  let user;
+  try {
+    user = JSON.parse(window.localStorage.getItem("user"));
+  } catch (error) {
+    console.log(error);
+  }
 
-  // Salvando o nome de usuário localmente
+  // Criando novo objeto caso user seja nulo
+  if (user == null) {
+    user = new Object();
+  }
+
+  // Recuperando o valor do nome do usuário
+  let nome = document.getElementById("nome");
+  if (user.nome) {
+    nome.value = user.nome;
+  }
+
+  // Recuperando o valor da senha do usuário
+  let senha = document.getElementById("senha");
+  if (user.senha) {
+    senha.value = user.senha;
+  }
+
+  // Salvando usuário localmente
   let login = document.getElementById("login");
   login.onsubmit = (evento) => {
-    console.log({ nome: nome.value, senha: senha.value });
-    window.localStorage.setItem("username", nome.value);
+    // Salvando nome de usuário
+    user.nome = nome.value;
+
+    // Salvando senha
+    user.senha = senha.value;
+
+    // Salvando usuário
+    window.localStorage.setItem("user", JSON.stringify(user));
+
+    // Impedindo que o formulário seja enviado
     evento.preventDefault();
   };
 
